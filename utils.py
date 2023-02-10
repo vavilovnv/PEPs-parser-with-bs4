c8b7a38da440183d3d22a90d0349075d7458f5c8
nike <vavilovnv@gmail.com>
2023-02-10 18:56:27 +0300

    Get PEP status func and LoggerWarning model added

--- utils.py
+++ utils.py
@@ -1,8 +1,16 @@
 import logging
 
+from bs4 import BeautifulSoup
 from requests import RequestException
+from pydantic import BaseModel, HttpUrl
 
 from exceptions import ParserFindTagException
+
+
+class LoggerWarning(BaseModel):
+    status: str
+    short_status: str
+    url: HttpUrl
 
 
 def get_response(session, url):
@@ -24,3 +32,18 @@ def find_tag(soup, tag, attrs=None):
         logging.error(error_msg, stack_info=True)
         raise ParserFindTagException(error_msg)
     return searched_tag
+
+
+def get_pep_status(session, url):
+    status = ''
+    if not url:
+        return status
+    response = get_response(session, url)
+    pep_soup = BeautifulSoup(response.text, features='lxml')
+    dl_div = find_tag(pep_soup, 'dl')
+    dt_divs = BeautifulSoup.find_all(dl_div, 'dt')
+    for dt_div in dt_divs:
+        if dt_div.text == 'Status:':
+            status = dt_div.find_next_sibling().string
+            break
+    return status
