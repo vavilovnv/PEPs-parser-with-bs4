e56d048c619ae6226414403bdfbb62636e3819b9
nike <vavilovnv@gmail.com>
2023-02-10 18:57:00 +0300

    Expected statuses constant added

--- constants.py
+++ constants.py
@@ -3,4 +3,15 @@ from pathlib import Path
 
 BASE_DIR = Path(__file__).parent
 MAIN_DOC_URL = 'https://docs.python.org/3/'
+PEPS_URL = 'https://peps.python.org/'
 DATETIME_FORMAT = '%Y-%m-%d_%H-%M-%S'
+EXPECTED_STATUSES = {
+    'A': ('Active', 'Accepted'),
+    'D': ('Deferred',),
+    'F': ('Final',),
+    'P': ('Provisional',),
+    'R': ('Rejected',),
+    'S': ('Superseded',),
+    'W': ('Withdrawn',),
+    '': ('Draft', 'Active'),
+}
