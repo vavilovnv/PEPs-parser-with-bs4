0f21ad0c36bb3247df7507cdc520ecb4048058a0
nike <vavilovnv@gmail.com>
2023-02-11 10:10:25 +0300

    PEP8 warning fix

--- src/constants.py
+++ src/constants.py
@@ -1,9 +1,9 @@
 from pathlib import Path
 
 
-BASE_DIR: Path = Path(__file__).parent
-MAIN_DOC_URL= 'https://docs.python.org/3/'
-PEPS_URL= 'https://peps.python.org/'
+BASE_DIR = Path(__file__).parent
+MAIN_DOC_URL = 'https://docs.python.org/3/'
+PEPS_URL = 'https://peps.python.org/'
 DATETIME_FORMAT = '%Y-%m-%d_%H-%M-%S'
 EXPECTED_STATUS = {
     'A': ('Active', 'Accepted'),
