f29bfd0246118c570dfbb4855d777cf45113c9d8
nike <vavilovnv@gmail.com>
2023-02-11 10:06:13 +0300

    Type hint fix

--- src/constants.py
+++ src/constants.py
@@ -2,10 +2,10 @@ from pathlib import Path
 
 
 BASE_DIR: Path = Path(__file__).parent
-MAIN_DOC_URL: str = 'https://docs.python.org/3/'
-PEPS_URL: str = 'https://peps.python.org/'
-DATETIME_FORMAT: str = '%Y-%m-%d_%H-%M-%S'
-EXPECTED_STATUS: dict[str, tuple[str]] = {
+MAIN_DOC_URL= 'https://docs.python.org/3/'
+PEPS_URL= 'https://peps.python.org/'
+DATETIME_FORMAT = '%Y-%m-%d_%H-%M-%S'
+EXPECTED_STATUS = {
     'A': ('Active', 'Accepted'),
     'D': ('Deferred',),
     'F': ('Final',),
